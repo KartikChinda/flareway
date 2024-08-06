6e3e0d04e997591dedac8b0ac08e6dde8d3dd4fe
Kartik Chinda <kartik.chinda@gmail.com>
2024-08-06 23:18:56 +0530

    Added the foundation for the students modal.

--- app/students/page.tsx
+++ app/students/page.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 
 const page = () => {
+
+    const [students, setstudents] = useState<any[]>([])
+
+    useEffect(() => {
+
+        const getStudents = async () => {
+            const response = await fetch("api/posts");
+            const studentsData = await response.json();
+            console.log(studentsData);
+            setstudents(studentsData);
+        }
+
+        getStudents();
+
+    }, [])
+
+
+
     return (
         <div>
             <Navbar />
+            <h2 className="text-xl p-2 font-semibold text-palette-2">
+                Hi, super glad to have you here.<br />
+                Below is a list of our stellar scholars.
+                <span className="block font-light text-lg mt-2">
+                    All their crypto addresses are listed below, as well as their bios. Feel free to splurge :)
+                </span>
+
+            </h2>
         </div>
     )
 }
--- steps.md
+++ steps.md
@@ -24,8 +24,9 @@
 
         2. Adding in the API route where this needs to be sent. Checking it up on prisma studio by running npx prisma studio. 
 
-3. Creating the API route, and then the frontend for displaying our students. 
+3. Creating the API route to fetch students, and then the frontend for displaying our students. 
 
 
+--- IF YOU DONT WANT TO ADD THE WALL, JUST ADD SOCIALS. KEEP THIS FOR THE LAST, DEPENDING ON THE TIME SAVED. 
 
 Add made with a keyboard at the end. Also add a list of dependencies when you start the project. 
